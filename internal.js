19203aeea836d926e63dbc9630914746f93bb059
Devin Howard <devin@callysto.com>
2017-01-09 00:37:56 -0500

    add xmpp notifications

--- internal.js
+++ internal.js
@@ -1,9 +1,14 @@
+require('isomorphic-fetch')
+const base64 = require('base-64')
+
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('messages.sqlite')
 
 const Voxbone = require('voxbone-voxsms')
 const { apiLogin, apiPassword } = require('./secrets')
 const voxbone = new Voxbone({ apiLogin, apiPassword })
+
+const { useXmpp, xmppUser, xmppPassword, xmppUrl, xmppPort, xmppMappings } = require('./secrets')
 
 function sanitize(msg) {
   return msg.replace('"', '\"')
@@ -13,6 +18,29 @@ function getDate() {
   return (new Date().getTime() / 1000).toFixed(0)
 }
 
+function notifyXmpp(msg, to) {
+  if (!useXmpp) return
+
+  // http://xmpp.verdexus.com:5280/msg/devin@xmpp.verdexus.com
+  const who = xmppMappings[to]
+  const url = `http://${xmppUrl}:${xmppPort}/msg/${who}@${xmppUrl}`
+
+  fetch('http://xmpp.verdexus.com:5280/msg/devin@xmpp.verdexus.com', {i
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + base64.encode(`${xmppUser}@${xmppUrl}:${xmppPassword}`),
+      'Content-Type': 'text/plain'
+    },
+    body: `You have a new message for ${to}: ${msg}`
+  }).then(response => {
+    return response.text()
+  }).then(payload => {
+    console.log(`XMPP notified for did ${to}`)
+  }).catch(error => {
+    console.log(`XMPP notification failed for did ${from}`)
+  })
+}
+
 function send(to, from, msg, dr = 'none') {
   const fragref = voxbone.createFragRef()
   voxbone.sendSMS(to, `+${from}`, msg, fragref)
@@ -20,6 +48,7 @@ function send(to, from, msg, dr = 'none') {
 }
 
 function receive(to, from, msg) {
+  notifyXmpp(msg, to)
   db.run(`INSERT INTO messages VALUES ('${to}', '${from}', ${getDate()}, "${sanitize(msg)}")`)
 }
 
